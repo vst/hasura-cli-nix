37c99e7b2472673f5fa0f0d7cd9e051f1b4498c7
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2025-10-13 09:51:05 +0800

    feat: bump Hasura CLI to v2.48.5

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ let
   ## Define the Hasura version:
   ##
   ## Note: If the Hasura version changes, `cli-ext.patch` may need to change, too.
-  version = "2.46.0";
+  version = "2.48.5";
 
   ## Get the OS and architecture:
   os = if stdenv.isDarwin then "darwin" else "linux";
@@ -21,7 +21,7 @@ let
     owner = "hasura";
     repo = "graphql-engine";
     rev = "v${version}";
-    sha256 = "sha256-J5srq7/duHKirXkluSg72/gKrRLVGtDmS4faX9xB10A=";
+    sha256 = "sha256-3mKwhRDmsI2fo5hd6DM3/+Nmf4eOswE2RUcOieOeSCE=";
   };
 
   ## Build the Hasura CLI extension:
